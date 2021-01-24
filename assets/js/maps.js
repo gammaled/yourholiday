80f66b5fa1b2124d2d232ff3f69c4c73b4ea26fa
gammaled <guleduni@yahoo.co.uk>
2021-01-24 22:55:34 +0000

    defined initMap function

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,27 +1,9 @@
+
+let map;
+
 function initMap() {
-
-    // load the map
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 51.5074, lng: 0.1278},
-        zoom: 4,
-        styles: mapStyle
-    });
-
-    var mapStyle = [{
-        'featureType': 'all',
-        'elementType': 'all',
-        'stylers': [{'visibility': 'off'}]
-    }, {
-        'featureType': 'landscape',
-        'elementType': 'geometry',
-        'stylers': [{'visibility': 'on'}, {'color': '#fcfcfc'}]
-    }, {
-        'featureType': 'water',
-        'elementType': 'labels',
-        'stylers': [{'visibility': 'off'}]
-    }, {
-        'featureType': 'water',
-        'elementType': 'geometry',
-        'stylers': [{'visibility': 'on'}, {'hue': '#5f94ff'}, {'lightness': 60}]
-    }];
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 51.5074, lng: 0.1278 },
+    zoom: 8,
+  });
 }
