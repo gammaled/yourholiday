e25958f6f83f4371edc9983d58c0ff63651c42b1
gammaled <guleduni@yahoo.co.uk>
2021-01-24 18:32:05 +0000

    changed map center to London

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,7 +2,7 @@ function initMap() {
 
     // load the map
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40, lng: -100},
+        center: {lat: 51.5074, lng: 0.1278},
         zoom: 4,
         styles: mapStyle
     });
