1a5307bf2858167a6acd2162d2186379bb9b8d24
gammaled <guleduni@yahoo.co.uk>
2021-02-04 13:44:59 +0000

    pasted in initMap() function from Google Place Searches and adjusted default center location to London

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -99,3 +99,32 @@
 }
 
 */
+
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+  var london = new google.maps.LatLng(51.5074, -0.127);
+
+  infowindow = new google.maps.InfoWindow();
+
+  map = new google.maps.Map(
+      document.getElementById('map'), {center: london, zoom: 14});
+
+  var request = {
+    query: 'Museum of Contemporary Art Australia',
+    fields: ['name', 'geometry'],
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+
+  service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
